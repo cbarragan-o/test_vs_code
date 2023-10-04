00e581d3c267fb21ddfcda46a8070f170adbcc8b
Barragan-Orozco <cbarragan-o@cps.k12.il.us>
2023-10-04 08:56:11 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# data abstraction when you need to simplify data
+# from something more complex
+# an example of data abstraction is when I use a list to simplify data
+# instaed of having to write out the same code over and over again
+# data abstraction is when you simplify data
+# if you have a list of names, you can simplify it by using a list
